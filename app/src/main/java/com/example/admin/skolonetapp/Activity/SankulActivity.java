88dd539fa9509f296a0578bdb564326b23fbb703
Kush <kushshah1234.ks@gmail.com>
2018-01-06 05:43:45 +0530

    commit work start the work and made the form.

---
+++ app/src/main/java/com/example/admin/skolonetapp/Activity/SankulActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.skolonetapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.skolonetapp.R;
+
+public class SankulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sankul);
+    }
+}
