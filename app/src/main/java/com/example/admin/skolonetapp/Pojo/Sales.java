e97d274e4df86d59f117fa067b0c6fb6907f37a1
yousoftech <arpitshah@yousoftech.com>
2018-01-21 03:33:55 +0530

    Initial Commit project setup

---
+++ app/src/main/java/com/example/admin/skolonetapp/Pojo/Sales.java
@@ -0,0 +1,280 @@
+package com.example.admin.skolonetapp.Pojo;
+
+/**
+ * Created by Admin on 21-01-2018.
+ */
+
+public class Sales {
+    public String PartyInfoId;;
+
+
+    public String PartyName;
+
+
+    public String OrganisationName;
+
+
+    public String Designation;
+
+
+    public String SankulName;
+
+
+    public String ShopName;
+
+
+    public String DistubitorName;
+
+    public String DistubitorType;
+
+    public int iUserTypeId;
+
+    public String ContactNo;
+
+    public int Board;
+
+    public int Medium;
+
+    public int Std;
+
+    public int AvgStudent;
+
+    public String getPartyInfoId() {
+        return PartyInfoId;
+    }
+
+    public void setPartyInfoId(String partyInfoId) {
+        PartyInfoId = partyInfoId;
+    }
+
+    public String getPartyName() {
+        return PartyName;
+    }
+
+    public void setPartyName(String partyName) {
+        PartyName = partyName;
+    }
+
+    public String getOrganisationName() {
+        return OrganisationName;
+    }
+
+    public void setOrganisationName(String organisationName) {
+        OrganisationName = organisationName;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+
+    public String getSankulName() {
+        return SankulName;
+    }
+
+    public void setSankulName(String sankulName) {
+        SankulName = sankulName;
+    }
+
+    public String getShopName() {
+        return ShopName;
+    }
+
+    public void setShopName(String shopName) {
+        ShopName = shopName;
+    }
+
+    public String getDistubitorName() {
+        return DistubitorName;
+    }
+
+    public void setDistubitorName(String distubitorName) {
+        DistubitorName = distubitorName;
+    }
+
+    public String getDistubitorType() {
+        return DistubitorType;
+    }
+
+    public void setDistubitorType(String distubitorType) {
+        DistubitorType = distubitorType;
+    }
+
+    public int getiUserTypeId() {
+        return iUserTypeId;
+    }
+
+    public void setiUserTypeId(int iUserTypeId) {
+        this.iUserTypeId = iUserTypeId;
+    }
+
+    public String getContactNo() {
+        return ContactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        ContactNo = contactNo;
+    }
+
+    public int getBoard() {
+        return Board;
+    }
+
+    public void setBoard(int board) {
+        Board = board;
+    }
+
+    public int getMedium() {
+        return Medium;
+    }
+
+    public void setMedium(int medium) {
+        Medium = medium;
+    }
+
+    public int getStd() {
+        return Std;
+    }
+
+    public void setStd(int std) {
+        Std = std;
+    }
+
+    public int getAvgStudent() {
+        return AvgStudent;
+    }
+
+    public void setAvgStudent(int avgStudent) {
+        AvgStudent = avgStudent;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        AddressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return AddressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        AddressLine2 = addressLine2;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String cityName) {
+        CityName = cityName;
+    }
+
+    public String getStateName() {
+        return StateName;
+    }
+
+    public void setStateName(String stateName) {
+        StateName = stateName;
+    }
+
+    public String getRemark() {
+        return Remark;
+    }
+
+    public void setRemark(String remark) {
+        Remark = remark;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public Boolean gettIsActive() {
+        return tIsActive;
+    }
+
+    public void settIsActive(Boolean tIsActive) {
+        this.tIsActive = tIsActive;
+    }
+
+    public String getDatetimeCreated() {
+        return datetimeCreated;
+    }
+
+    public void setDatetimeCreated(String datetimeCreated) {
+        this.datetimeCreated = datetimeCreated;
+    }
+
+    public String getDatetimeUpdated() {
+        return datetimeUpdated;
+    }
+
+    public void setDatetimeUpdated(String datetimeUpdated) {
+        this.datetimeUpdated = datetimeUpdated;
+    }
+
+    public String getCreatedBy() {
+        return CreatedBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        CreatedBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return UpdatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        UpdatedBy = updatedBy;
+    }
+
+    public int getiPartyTypeId() {
+        return iPartyTypeId;
+    }
+
+    public void setiPartyTypeId(int iPartyTypeId) {
+        this.iPartyTypeId = iPartyTypeId;
+    }
+
+    public String AddressLine1;
+
+
+    public String AddressLine2;
+
+
+    public String CityName;
+
+
+    public String StateName;
+
+
+    public String Remark;
+
+
+    public String Location;
+
+    public Boolean tIsActive;
+
+    public String datetimeCreated;
+
+    public String datetimeUpdated;
+
+    public String CreatedBy;
+
+    public String UpdatedBy;
+
+    public int iPartyTypeId;
+
+
+}
