b092636b5de443c961d7a69e74e1c20dfec4c8e3
yousoftech <arpitshah@yousoftech.com>
2018-02-10 17:38:49 +0530

    Initial Commit project setup

---
+++ app/src/main/java/com/example/admin/skolonetapp/Adapter/adaptercomment.java
@@ -0,0 +1,37 @@
+package com.example.admin.skolonetapp.Adapter;
+
+/**
+ * Created by Admin on 10-02-2018.
+ */
+
+public class adaptercomment {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    int id;
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    String remark;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    String text;
+
+}
---
+++ app/src/main/java/com/example/admin/skolonetapp/Pojo/comment.java
@@ -0,0 +1,27 @@
+package com.example.admin.skolonetapp.Pojo;
+
+/**
+ * Created by Admin on 10-02-2018.
+ */
+
+public class comment {
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public String s;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int i;
+}
